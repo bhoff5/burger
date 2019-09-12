624380cc89acd049bbca764b443e373ceffa8114
Brian Hoff <blhoff52002@gmail.com>
2019-09-12 15:59:55 -0500

    adding bootstrap, adding routing

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,4 @@
-var connection = require("../config/connection.js");
+var connection = require("./connection.js");
 
 function printQuestionMarks(num) {
   var arr = [];
@@ -29,10 +29,13 @@ function objToSql(ob) {
 
 var orm = {
   selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+
     connection.query(queryString, function(err, result) {
       if (err) {
         throw err;
       }
+
       cb(result);
     });
   },
